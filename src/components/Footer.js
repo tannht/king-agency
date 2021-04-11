dbbeec12207a3fa7bb2e88d5fcf396330b79a014
tannht <hoangtan.nht@gmail.com>
2021-04-11 12:39:18 +0700

    update
    update

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Footer() {
+  return (
+    <div >
+      Footer | HELLO KAGENCY
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header() {
+  return (
+    <div >
+      Header | HELLO KAGENCY
+    </div>
+  );
+}
+
+export default Header;

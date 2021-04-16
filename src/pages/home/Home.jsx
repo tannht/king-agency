57a3a325b5d4bb56f0ab7b648f831f9743fb7e54
Thang Nguyen Cao Ngoc <c.thang.nguyen@zonarsystems.com>
2021-04-16 16:55:11 +0700

    clients logo component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,12 +4,14 @@ import Projects from "./components/Projects/Projects";
 import News from "./components/News";
 import Info from "./components/Info";
 import "./Home.scss";
+import ClientsLogo from "../../components/ClientsLogo";
 function Home() {
   return (
     <div className="Home container">
       <Banner />
       <Info />
       <Projects />
+      <ClientsLogo />
       <News />
     </div>
   );

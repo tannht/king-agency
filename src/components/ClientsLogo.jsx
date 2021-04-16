7f4afdd39220a97a2abe42beac29c2c09065bef8
Thang Nguyen <caothang1183@gmail.com>
2021-04-16 15:19:18 +0700

    Project Component

--- src/components/ClientsLogo.jsx
+++ src/components/ClientsLogo.jsx
@@ -1,3 +1,12 @@
 import React, {Component} from "react";
 
+const ClientsLogo = () => (
+    <div className="ClientLogo">
+        <h1>Our Clients</h1>
+        <div className="row">
+            
+        </div>
+    </div>
+);
 
+export default ClientsLogo;

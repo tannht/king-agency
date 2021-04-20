b400b9a2533f7785bfd1561b2f72c209357dfdbb
Thang Nguyen <caothang1183@gmail.com>
2021-04-20 11:37:26 +0700

    about page 60%

--- src/components/PageDescription.jsx
+++ src/components/PageDescription.jsx
@@ -5,12 +5,12 @@ const PageDescription = ({ title, titleContent, content }) => {
     <div className="PageDescription">
       <div className="container">
         <div className="row">
-          <div className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+          <div className="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
             <div className="title">
               <h1>{title}</h1>
             </div>
           </div>
-          <div className="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 content">
+          <div className="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 content">
             <h3>{titleContent}</h3>
             <p>{content}</p>
           </div>
--- src/pages/about_us/AboutUs.jsx
+++ src/pages/about_us/AboutUs.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import "./AboutUs.scss";
+import PageDescription from "../../components/PageDescription";
+import AboutVideo from "./components/AboutVideo";
+import AboutDescription from "./components/AboutDescription";
+import AboutImages from "./components/AboutImages";
+import ClientsLogo from "../../components/ClientsLogo";
+
 
 const AboutUs = () => (
-  <div>
-    <h1>About Us Page</h1>
+  <div className="AboutUs">
+    <div className="container">
+      <PageDescription
+        title="About Us"
+        titleContent="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+        content="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web"
+      />
+      <AboutVideo />
+      <AboutDescription />
+      <AboutImages />
+    </div>
+    <ClientsLogo />
   </div>
 );
 

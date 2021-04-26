3f21697cccc9172e4c10042609389d3ee2798e8d
thind9x <thind9x@gmail.com>
2021-04-26 14:04:03 +0700

    Privacy & Term Modal
    thi_privacy_term_page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,81 @@
 import { useState } from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
 import SubMenu from "./sub_components/SubMenu";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import ytGrey from '../assets/images/icons/yt_grey.svg';
 import pinGrey from '../assets/images/icons/pin_grey.svg';
 import insGrey from '../assets/images/icons/ins_grey.svg';
 import inlGrey from '../assets/images/icons/inl_grey.svg';
 import LogoIcon from '../assets/images/logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import { FaTimes } from "react-icons/fa";
 
+
+import {IconButton,Toolbar,AppBar,Button,Slide} from '@material-ui/core';
+import Typography from '@material-ui/core/Typography'
+import CloseIcon from '@material-ui/icons/Close';
+
+import PrivacyPolicy from '../pages/privacy_policy/PrivacyPolicy';
+import Term from '../pages/privacy_policy/Term';
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+    alignItems: 'center',
+    backgroundColor:'white'
+
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+  container: {
+    height: 200,
+    position: "relative",
+    
+  },
+  
+  buttoncenter: {
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+
+  
+  }
+}));
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="bottom" ref={ref} {...props} />;
+});
+
+const TransitionTerm = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 const Footer = ({ hiddenContact = false }) => {
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [openterm, setOpenTerm] = React.useState(false);
+
+  const handleClickOpen = (event) => {
+    setOpen(true);
+  };
+
+  const handleClose = (event) => {
+    setOpen(false);
+  };
+
+  const handleClickOpenTerm = (event) => {
+    setOpenTerm(true);
+  };
+
+  const handleCloseTerm = (event) => {
+    setOpenTerm(false);
+  };
+
+
 
   return (
     <div className="footer ">
@@ -59,12 +124,12 @@ const Footer = ({ hiddenContact = false }) => {
             <div className="col-12 col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
               <ul>
                 <li>
-                  <Link to="#" onClick={toggle}>
+                  <Link to="#" onClick={handleClickOpen}>
                     Privacy Policy
                   </Link>
                 </li>
                 <li>
-                  <Link to="#">Terms and Conditions</Link>
+                  <Link onClick={handleClickOpenTerm} to="#">Terms and Conditions</Link>
                 </li>
               </ul>
             </div>
@@ -72,26 +137,39 @@ const Footer = ({ hiddenContact = false }) => {
         </div>
       </div>
 
-      <Modal
-        isOpen={modal}
-        toggle={toggle}
-        modalTransition={{ timeout: 200 }}
-        backdropTransition={{ timeout: 200 }}
-      >
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          scelerisque nisl dolor,
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Modal>
+    {/* Start of Chính sách quyền riêng tư Modal*/}
+
+      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <AppBar color="secondary"  className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+            <FaTimes style={{color:'black'}}  />
+            </IconButton>
+            
+           
+          </Toolbar>
+        </AppBar>
+       
+      <PrivacyPolicy/>
+      </Dialog>
+    {/* End of Chính sách quyền riêng tư Modal*/}
+
+    {/* Start of Điều khoản dịch dụ Modal*/}
+      <Dialog fullScreen open={openterm} onClose={handleCloseTerm} TransitionComponent={TransitionTerm}>
+        <AppBar color="secondary"  className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleCloseTerm} aria-label="close">
+              <FaTimes style={{color:'black'}}  />
+            </IconButton>
+            
+          
+          </Toolbar>
+        </AppBar>
+       
+      <Term/>
+      </Dialog>
+         {/* End of Điều khoản dịch dụ Modal*/}
+
     </div>
   );
 };
